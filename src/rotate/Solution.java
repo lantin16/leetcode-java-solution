97a526ffcd4ab360d66d3e416c9cff23dfaadae7
lantin16 <xzc1693813596@163.com>
2024-03-07 21:43:56 +0800

    No.189 轮转数组（未解决）

---
+++ src/rotate/Solution.java
@@ -0,0 +1,14 @@
+package rotate;
+
+/**
+ * leetcode No.189 轮转数组
+ *
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ */
+public class Solution {
+
+    public void rotate(int[] nums, int k) {
+        // 通过部分反转再拼接来坐
+        // 注意轮转超过长度的倍数均相当于没轮转，所以只需要轮转k取模后的数
+    }
+}
