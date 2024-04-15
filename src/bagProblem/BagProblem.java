cfcc80b726953d99a07e73c7879b132d1f42cb45
lantin16 <xzc1693813596@163.com>
2024-04-15 22:10:05 +0800

    完全背包问题

--- src/bagProblem/BagProblem.java
+++ src/bagProblem/BagProblem.java
@@ -62,6 +62,7 @@ public class BagProblem {
      * 01背包问题
      * 动态规划 + 一维滚动数组，优化空间复杂度
      * 代码更简洁，空间复杂度降低了一个量级
+     * 需要保证遍历j时，j之后的都是本轮i计算的，j及之前的都是上一轮i的
      * @param weight 物品的重量
      * @param value 物品的价值
      * @param bagSize 背包的容量
@@ -85,6 +86,60 @@ public class BagProblem {
                 dp[j] = Math.max(dp[j], dp[j-weight[i]] + value[i]);
             }
         }
+
+        // 5. 打印dp数组
+        for (int j = 0; j <= bagSize; j++) {
+            System.out.print(dp[j] + " ");
+        }
+
+        // 最后dp[bagSize]即为所求
+    }
+
+
+
+    /**
+     * 完全背包问题，每种物品的数量无限，可重复选择
+     * 动态规划 + 一维滚动数组
+     * 需要保证遍历j时，j之前的都是本轮i计算的，j及之后的都是上一轮i的
+     * 完全背包问题相较于01背包问题：
+     * 1. 遍历背包容量时得顺序遍历，因为一种物品可重复添加多次
+     * 2. 先遍历物品还是先遍历背包都可以，因为dp[j] 是根据 下标j及之前所对应的dp[j]计算出来的。 只要保证下标j之前的dp[j]都是经过计算的就可以了。
+     * @param weight 物品的重量
+     * @param value 物品的价值
+     * @param bagSize 背包的容量
+     */
+    public static void CompletePack(int[] weight, int[] value, int bagSize) {
+        int goods = weight.length;  // 物品的数量
+        // 1. dp[j]表示容量为j的背包，所背的物品价值可以最大为dp[j]
+        int[] dp = new int[bagSize + 1];
+
+        // 2. 递推公式：
+        // 二维的形式：dp[i][j] = max{dp[i-1][j], dp[i][j-weight[i]] + value[i]]}，注意这里第二项是dp[i][j-weight[i]] + value[i]]而不是01背包的dp[i-1][j-weight[i]] + value[i]]
+        // dp[i][j]有两种来源：下标的i的物品不放进去，或者容量j空间足够把i物品放进去，剩下就从0到i的物品中选择放入容量为j-weight[i]的背包（即使本次放入i，前面j-weight[i]的空间也可能有i）
+        // 一维的形式：dp[j] = max{dp[j], dp[j-weight[i]] + value[i]]}
+
+        // 3. 初始化
+        // 全部初始化为0即可，下面遍历就从i=0开始遍历
+
+        // 4. 遍历顺序，先遍历物品再遍历背包容量或先遍历背包容量再遍历物品都可以
+        // 如先遍历物品再遍历背包容量
+        for (int i = 0; i < goods; i++) {   // 注意dp数组全初始化为0这里的i就得从0开始
+            // j只能顺序遍历，因为一种物品可重复添加多次
+            for (int j = weight[i]; j <= bagSize; j++) {  // 注意：这里j从weight[i]开始遍历，j<weight[i]时物品i就放不进去了，此时保持上一行的dp[j]即可
+                dp[j] = Math.max(dp[j], dp[j-weight[i]] + value[i]);
+            }
+        }
+
+
+//        // 也可以先遍历背包容量再遍历物品
+//        for (int j = 1; j <= bagSize; j++) {    // 这里j从1开始即可，因为j=0即背包容量为0时是放不进任何物品的，因此第0列的都是0
+//            for (int i = 0; i < goods; i++) {
+//                if (j >= weight[i]) {
+//                    dp[j] = Math.max(dp[j], dp[j-weight[i]] + value[i]);
+//                }
+//            }
+//        }
+
 
         // 5. 打印dp数组
         for (int j = 0; j <= bagSize; j++) {
