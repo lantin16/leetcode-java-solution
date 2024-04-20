1b7cfa288511442f65619046f04137a737c7b8e7
lantin <xzc1693813596@163.com>
2024-04-20 20:26:36 +0800

    No.300 最长递增子序列

--- src/lengthOfLIS/Solution.java
+++ src/lengthOfLIS/Solution.java
@@ -19,39 +19,39 @@ public class Solution {
      * @param nums
      * @return
      */
-    // // 时间复杂度：O(n^2)，其中n为nums的长度。分析：求每个dp[i]都要将i之前的所有j都遍历一遍。
-    // // 空间复杂度：O(n)，其中n为nums的长度
-    // public int lengthOfLIS(int[] nums) {
-    //     int n = nums.length;
-    //
-    //     // dp[i]：从下标0~i中挑选的以nums[i]结尾的最长严格递增子序列的长度
-    //     // 为什么强调以nums[i]结尾而不是直接设为0~i中的最长严格递增子序列的长度？
-    //     // —— 因为递增比较比的是两个元素，来判断是一个元素是否能接在另一个元素之后严格递增，如果是递增的，那么后一个元素结尾的最长递增序列就要考虑接在较小元素后面的情况
-    //     int[] dp = new int[n];
-    //
-    //     // 递推公式：dp[i] = max{dp[j] + 1}，j < i且nums[i] > nums[j]
-    //     // 考察i前面的每一个满足 nums[j] < nums[i] 的情况，这表示nums[i]可以接在nums[j]的后面，那么这种情况的最长递增序列长度就等于dp[j] + 1，1就是nums[i]
-    //
-    //     // 初始化
-    //     Arrays.fill(dp, 1); // 以每个元素为结尾的严格递增子序列至少是它自己
-    //
-    //     int maxLen = 1;
-    //
-    //     // 遍历顺序：从前往后
-    //     for (int i = 1; i < n; i++) {
-    //         for (int j = 0; j < i; j++) {
-    //             if (nums[i] > nums[j]) {
-    //                 dp[i] = Math.max(dp[i], dp[j] + 1);
-    //             }
-    //         }
-    //
-    //         if (dp[i] > maxLen) {
-    //             maxLen = dp[i];
-    //         }
-    //     }
-    //
-    //     return maxLen;  // 最后nums数组的最长严格递增子序列可能出现在以其中任一个元素结尾，因此结果为max{dp[i]}，这里用maxLen变量在计算每个dp[i]的同时维护好了
-    // }
+    // 时间复杂度：O(n^2)，其中n为nums的长度。分析：求每个dp[i]都要将i之前的所有j都遍历一遍。
+    // 空间复杂度：O(n)，其中n为nums的长度
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+
+        // dp[i]：从下标0~i中挑选的以nums[i]结尾的最长严格递增子序列的长度
+        // 为什么强调以nums[i]结尾而不是直接设为0~i中的最长严格递增子序列的长度？
+        // —— 因为递增比较比的是两个元素，来判断是一个元素是否能接在另一个元素之后严格递增，如果是递增的，那么后一个元素结尾的最长递增序列就要考虑接在较小元素后面的情况
+        int[] dp = new int[n];
+
+        // 递推公式：dp[i] = max{dp[j] + 1}，j < i且nums[i] > nums[j]
+        // 考察i前面的每一个满足 nums[j] < nums[i] 的情况，这表示nums[i]可以接在nums[j]的后面，那么这种情况的最长递增序列长度就等于dp[j] + 1，1就是nums[i]
+
+        // 初始化
+        Arrays.fill(dp, 1); // 以每个元素为结尾的严格递增子序列至少是它自己
+
+        int maxLen = 1;
+
+        // 遍历顺序：从前往后
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+            }
+        }
+
+        return maxLen;  // 最后nums数组的最长严格递增子序列可能出现在以其中任一个元素结尾，因此结果为max{dp[i]}，这里用maxLen变量在计算每个dp[i]的同时维护好了
+    }
 
 
 
@@ -71,40 +71,40 @@ public class Solution {
      * @param nums
      * @return
      */
-    // 时间复杂度：O(nlogn)，其中n为nums的长度。
-    // 空间复杂度：O(n)，其中n为nums的长度
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-
-        // tails[i]：所有长度为i+1的递增子序列的末尾元素的最小值
-        // 数组tails是严格递增的，反证法可证
-        // 理解：将各种长度的上升子序列的增长最慢的情况记录下来
-        int[] tails = new int[n];
-        tails[0] = nums[0];
-        int len = 1;    // 记录目前最长上升子序列的长度
-
-
-        // 从前往后遍历每一个元素，并维护tails数组的递增性
-        for (int i = 1; i < n; i++) {
-            // 如果遇到一个比 tails 的尾部元素更大的值，说明形成了一个更长的上升序列。 则把它追加到 tails 的尾部。
-            if (nums[i] > tails[len-1]) {   // 长度len对于tails数组的下标len-1
-                len++;
-                tails[len - 1] = nums[i];
-            } else {    // 如果遇到一个比 tails 的尾部元素更小的值，说明发现了某个上升子序列的、更小的末尾元素，需要更新它（如果相等的话按照下面的代码会更新同样的值，不影响结果）
-                // 在 tails 数组中二分查找，找到第一个比nums[i]小的数 tails[pos] ，并更新 tails[pos+1]=nums[i]
-                int l = 0, r = len-1, pos = -1;    // pos指向tails中第一个比nums[i]小的数。如果找不到比nums[i]小的tails元素，则应该更新tails[0]，故pos初始化为-1
-                while (l <= r) {
-                    int mid = l + (r-l)/2;  // 中间或偏左
-                    if (nums[i] > tails[mid]) { // 至少找到一个比nums[i]的了，先更新pos到这里
-                        pos = mid;
-                        l = mid + 1;
-                    } else {
-                        r = mid - 1;
-                    }
-                }
-                tails[pos + 1] = nums[i];   // 更新的是pos+1
-            }
-        }
-        return len;
-    }
+    // // 时间复杂度：O(nlogn)，其中n为nums的长度。
+    // // 空间复杂度：O(n)，其中n为nums的长度
+    // public int lengthOfLIS(int[] nums) {
+    //     int n = nums.length;
+    //
+    //     // tails[i]：所有长度为i+1的递增子序列的末尾元素的最小值
+    //     // 数组tails是严格递增的，反证法可证
+    //     // 理解：将各种长度的上升子序列的增长最慢的情况记录下来
+    //     int[] tails = new int[n];
+    //     tails[0] = nums[0];
+    //     int len = 1;    // 记录目前最长上升子序列的长度
+    //
+    //
+    //     // 从前往后遍历每一个元素，并维护tails数组的递增性
+    //     for (int i = 1; i < n; i++) {
+    //         // 如果遇到一个比 tails 的尾部元素更大的值，说明形成了一个更长的上升序列。 则把它追加到 tails 的尾部。
+    //         if (nums[i] > tails[len-1]) {   // 长度len对于tails数组的下标len-1
+    //             len++;
+    //             tails[len - 1] = nums[i];
+    //         } else {    // 如果遇到一个比 tails 的尾部元素更小的值，说明发现了某个上升子序列的、更小的末尾元素，需要更新它（如果相等的话按照下面的代码会更新同样的值，不影响结果）
+    //             // 在 tails 数组中二分查找，找到第一个比nums[i]小的数 tails[pos] ，并更新 tails[pos+1]=nums[i]
+    //             int l = 0, r = len-1, pos = -1;    // pos指向tails中第一个比nums[i]小的数。如果找不到比nums[i]小的tails元素，则应该更新tails[0]，故pos初始化为-1
+    //             while (l <= r) {
+    //                 int mid = l + (r-l)/2;  // 中间或偏左
+    //                 if (nums[i] > tails[mid]) { // 至少找到一个比nums[i]的了，先更新pos到这里
+    //                     pos = mid;
+    //                     l = mid + 1;
+    //                 } else {
+    //                     r = mid - 1;
+    //                 }
+    //             }
+    //             tails[pos + 1] = nums[i];   // 更新的是pos+1
+    //         }
+    //     }
+    //     return len;
+    // }
 }
